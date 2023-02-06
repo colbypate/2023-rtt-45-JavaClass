62487463c8373a9103bda942be24bf10069a2f09
Colby Pate <cbpate08@gmail.com>
2023-02-06 16:01:37 -0600

    Finished first two questions on assignment

---
+++ ClassWork/Assignments/Week1/303.1.2/src/com/perscholas/java_basics/JavaBasicsClass.java
@@ -0,0 +1,19 @@
+package com.perscholas.java_basics;
+
+public class JavaBasicsClass {
+	public static void main(String[] args) {
+		int a = 8;
+		int b = 3;
+		int sumAB = a+b;
+		System.out.println("a + b = "+sumAB);
+		
+		int x = 7;
+		int y = 5;
+		int sumXY = x+y;
+		System.out.println("x + y = "+sumXY);
+		
+		
+		
+	}
+	
+}
