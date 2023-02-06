7a41bc2071cad8d833fd3dd43c9f54edc442c406
Colby Pate <cbpate08@gmail.com>
2023-02-06 16:15:50 -0600

    Added questions to the top of the program

--- ClassWork/Assignments/Week1/303.1.2/src/com/perscholas/java_basics/JavaBasicsClass.java
+++ ClassWork/Assignments/Week1/303.1.2/src/com/perscholas/java_basics/JavaBasicsClass.java
@@ -1,5 +1,40 @@
 package com.perscholas.java_basics;
 
+//Write a program that declares 2 integer variables, assigns an integer to each,
+//and adds them together. Assign the sum to a variable. Print out the result.
+//
+//Write a program that declares 2 double variables, assigns a number to each,
+//and adds them together. Assign the sum to a variable. Print out the result.
+// 
+//Write a program that declares an integer variable and a double variable,
+//assigns numbers to each, and adds them together. Assign the sum to a variable. 
+//Print out the result. What variable type must the sum be?
+// 
+//Write a program that declares 2 integer variables, assigns an integer to each,
+//and divides the larger number by the smaller number. Assign the result to a 
+//variable. Print out the result. Now change the larger number to a decimal.
+//What happens? What corrections are needed?
+// 
+//Write a program that declares 2 double variables, assigns a number to each,
+//and divides the larger by the smaller. Assign the result to a variable. 
+//Print out the result. Now, cast the result to an integer. Print out the
+//result again.
+// 
+//Write a program that declares two integer variables, x, and y, and assigns 5
+//to x and 6 to y. Declare a variable q and assign y/x to it and print q. 
+//Now, cast y to a double and assign it to q. Print q again.
+// 
+//Write a program that declares a named constant and uses it in a calculation.
+//Print the result.
+// 
+//Write a program where you create 3 variables that represent products at a cafe.
+//The products could be beverages like coffee, cappuccino, espresso, green tea, etc.
+//Assign prices to each product. Create 2 more variables called subtotal and
+//totalSale and complete an “order” for 3 items of the first product, 4 items of 
+//the second product, and 2 items of the third product. Add them all together to
+//calculate the subtotal. Create a constant called SALES_TAX and add sales tax 
+//to the subtotal to obtain the totalSale amount. Format the results to 2 decimal places.
+
 public class JavaBasicsClass {
 	public static void main(String[] args) {
 		int a = 8;
@@ -11,6 +46,7 @@ public class JavaBasicsClass {
 		int y = 5;
 		int sumXY = x+y;
 		System.out.println("x + y = "+sumXY);
+		
 		
 		
 		
