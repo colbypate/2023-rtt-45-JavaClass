98daf944b212ed4025cb5e92da6ed29af470407e
Colby Pate <cbpate08@gmail.com>
2023-02-07 15:25:35 -0600

    Started conditional statement assignment

---
+++ ClassWork/Assignments/Week2Java/303.2.2/src/practiceAssignment/ControlStatements.java
@@ -0,0 +1,15 @@
+package practiceAssignment;
+
+public class ControlStatements {
+	public static void main(String[] args) {
+		
+		
+	}
+	public void number1() {
+		int x = 7;
+		if(x<10) {
+			System.out.println("Less than 10");
+		}
+	}
+
+}
