5a38ab635aa9df014e3c8ddc5581c9fd202dd25e
Colby Pate <cbpate08@gmail.com>
2023-02-10 14:52:08 -0600

    Added new functionality to coffee shop (menu, cart)

--- ClassWork/Assignments/Week2Java/coffeeShop/src/coffeeShop/CoffeeShop.java
+++ ClassWork/Assignments/Week2Java/coffeeShop/src/coffeeShop/CoffeeShop.java
@@ -2,11 +2,21 @@ package coffeeShop;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class CoffeeShop {
 	
+	public static final int PRINT_MENU = 1;
+	public static final int ORDER_ITEM = 2;
+	public static final int VIEW_CART = 3;
+	public static final int EXIT = 4;
 	//	this private member variable can only be accessed inside the CoffeeShop class
 	private List<MenuItem> menuItems = new ArrayList<>();
+	
+	private List<MenuItem> cart = new ArrayList<>();
+	
+	//	all member variables are defined at the top of the file
+	private Scanner scanner = new Scanner(System.in);
 	
 	public void initialize() {
 		MenuItem item1 = new MenuItem();
@@ -41,7 +51,74 @@ public class CoffeeShop {
 			String.format("%.2f", item.getPrice()) + "\t" + 
 					+ item.getInStock());
 			}
+		System.out.println("----------\t-----\t-----------------");		
 		}
+	
+	public int menuPrompt() {
+		System.out.println("Welcome to Colby's Coffee Shop");
+		System.out.println(PRINT_MENU + ") Print Menu");
+		System.out.println(ORDER_ITEM + ") Order Item");
+		System.out.println(VIEW_CART + ") View Cart:");
+		System.out.println(EXIT + ") Exit Coffee Shop");
+
+		System.out.print("\nMake Selection: ");
+		
+		int selection = scanner.nextInt();
+		scanner.nextLine();
+		System.out.println("");
+		
+		//System.out.println("Opening menu "+selection);
+
+		return selection;
+		
+	}
+	
+	public void orderItem() {
+		printMenuItems();
+		System.out.print("Enter item name: ");
+		String itemName = scanner.nextLine();
+		System.out.println(itemName);
+		
+		MenuItem selectedItem = findMenuItemByItemName(itemName);
+		if(selectedItem != null) {
+			System.out.println(itemName + " is valid");
+			cart.add(selectedItem);
+		}else {
+			System.out.println(itemName + " is not a valid selection");
+		}
+	}
+	
+	//	this is a very common patter in software engineering---
+	//	if the method returns null then the itemName was not found
+	//	else if the method returns a MenuItemit was
+	private MenuItem findMenuItemByItemName(String itemName) {
+		if(itemName == null) {
+			return null;
+		}
+		itemName = itemName.trim();
+		for(MenuItem menuItem : menuItems) {
+			if(menuItem.getName().equalsIgnoreCase(itemName)) {
+				return menuItem;
+				
+			}
+		}
+		return null;
+	}
+	
+	public void viewCart() {
+		double totalPrice = 0.0;
+		System.out.println(cart.size() + " item(s) in your cart: \n");
+		System.out.println("Item Name\tPrice\t");
+		System.out.println("----------\t-----");
+		for(MenuItem item : cart) {
+			System.out.println(item.getName() + "\t$" + 
+			String.format("%.2f", item.getPrice()));
+			totalPrice  = totalPrice + item.getPrice();
+		}
+		System.out.println("----------\t-----\t");
+		System.out.println("Total Price:\t$" + String.format("%.2f", totalPrice));
+		System.out.println("");
+	}
 
 }
 
--- ClassWork/Assignments/Week2Java/coffeeShop/src/coffeeShop/CoffeeShopMain.java
+++ ClassWork/Assignments/Week2Java/coffeeShop/src/coffeeShop/CoffeeShopMain.java
@@ -6,8 +6,23 @@ public class CoffeeShopMain {
 	
 		CoffeeShop cs = new CoffeeShop();
 		cs.initialize();
-		cs.printMenuItems();
-
+		//cs.printMenuItems();
+		//cs.menuPrompt();
+		while(true) {
+			int selection = cs.menuPrompt();
+			if(selection == CoffeeShop.PRINT_MENU) {
+				cs.printMenuItems();
+			}if(selection == CoffeeShop.ORDER_ITEM) {
+				cs.orderItem();
+			}if(selection == CoffeeShop.VIEW_CART) {
+				cs.viewCart();
+			}if(selection == CoffeeShop.EXIT) {
+				System.out.println("Thanks for stopping by. See you next time!");
+				//	This tells the JVM to exit with a status code of successful
+				System.exit(0);
+			}
+		}
 	}
+
 
 }
