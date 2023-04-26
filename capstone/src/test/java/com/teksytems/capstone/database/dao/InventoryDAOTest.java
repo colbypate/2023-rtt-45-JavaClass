0f3d02c7d172a5fa30ac432fbac23316866fd154
Colby Pate <cbpate08@gmail.com>
2023-04-26 15:49:14 -0500

    added JUnit testing

---
+++ capstone/src/test/java/com/teksytems/capstone/database/dao/InventoryDAOTest.java
@@ -0,0 +1,49 @@
+package com.teksytems.capstone.database.dao;
+
+import com.teksytems.capstone.database.dao.InventoryDAO;
+import com.teksytems.capstone.database.entity.Inventory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
+import org.springframework.data.jpa.repository.Query;
+
+@SpringBootTest
+public class InventoryDAOTest {
+
+    @Autowired
+    private InventoryDAO inventoryDAO;
+
+    @Test
+    public void findById() {
+        inventoryDAO.findById(4);
+    }
+
+    @Test
+    @Order(0)
+    public void createInventory(){
+
+        Inventory given = new Inventory();
+        given.setProductName("test");
+        given.setQuantity(5);
+        given.setPrice(1.99);
+        given.setPhotoURL(null);
+        inventoryDAO.save(given);
+
+        Inventory actual = inventoryDAO.findInventoryByProductNameExact("test");
+
+        Assertions.assertEquals(given.getProductName(), actual.getProductName());
+        Assertions.assertEquals(given.getPrice(), actual.getPrice());
+        Assertions.assertEquals(given.getQuantity(), actual.getQuantity());
+        Assertions.assertEquals(given.getPhotoURL(), actual.getPhotoURL());
+
+
+
+
+    }
+
+}
